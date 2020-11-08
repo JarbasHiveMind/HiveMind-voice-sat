bd5b24cb9e95919453e0d5f8722a601caafe33f5
jarbasal <jarbasai@mailfence.com>
2020-11-08 21:07:33 +0000

    dead code remove

--- mycroft_voice_satellite/speech/mic.py
+++ mycroft_voice_satellite/speech/mic.py
@@ -177,56 +177,6 @@ def get_silence(num_bytes):
     return b'\0' * num_bytes
 
 
-def resolve_resource_file(res_name):
-    """Convert a resource into an absolute filename.
-
-    Resource names are in the form: 'filename.ext'
-    or 'path/filename.ext'
-
-    The system wil look for ~/.mycroft/res_name first, and
-    if not found will look at /opt/mycroft/res_name,
-    then finally it will look for res_name in the 'mycroft/res'
-    folder of the source code package.
-
-    Example:
-    With mycroft running as the user 'bob', if you called
-        resolve_resource_file('snd/beep.wav')
-    it would return either '/home/bob/.mycroft/snd/beep.wav' or
-    '/opt/mycroft/snd/beep.wav' or '.../mycroft/res/snd/beep.wav',
-    where the '...' is replaced by the path where the package has
-    been installed.
-
-    Args:
-        res_name (str): a resource path/name
-    Returns:
-        str: path to resource or None if no resource found
-    """
-    config = CONFIGURATION
-
-    # First look for fully qualified file (e.g. a user setting)
-    if os.path.isfile(res_name):
-        return res_name
-
-    # Now look for ~/.mycroft/res_name (in user folder)
-    filename = os.path.expanduser("~/.mycroft/" + res_name)
-    if os.path.isfile(filename):
-        return filename
-
-    # Next look for /opt/mycroft/res/res_name
-    data_dir = os.path.expanduser(config['data_dir'])
-    filename = os.path.expanduser(os.path.join(data_dir, res_name))
-    if os.path.isfile(filename):
-        return filename
-
-    # Finally look for it in the source package
-    filename = os.path.join(os.path.dirname(__file__), '..', 'res', res_name)
-    filename = os.path.abspath(os.path.normpath(filename))
-    if os.path.isfile(filename):
-        return filename
-
-    return None  # Resource cannot be resolved
-
-
 class ResponsiveRecognizer(speech_recognition.Recognizer):
     # Padding of silence when feeding to pocketsphinx
     SILENCE_SEC = 0.01
