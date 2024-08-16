ce70b4480feae53641c53565e27f3f721bbcd7ca
miro <jarbasai@mailfence.com>
2024-08-16 05:12:27 +0100

    performance/real_websocket

--- hivemind_voice_satellite/__main__.py
+++ hivemind_voice_satellite/__main__.py
@@ -5,12 +5,31 @@ from hivemind_bus_client import HiveMessageBusClient
 from hivemind_bus_client.identity import NodeIdentity
 from hivemind_ggwave import GGWaveSlave
 from ovos_audio.service import PlaybackService
+from ovos_bus_client.client import MessageBusClient
 from ovos_utils import wait_for_exit_signal
 from ovos_utils.log import init_service_logger, LOG
-
+from ovos_utils.fakebus import FakeBus
 from hivemind_voice_satellite import VoiceClient
 
 
+def launch_bus_daemon() -> MessageBusClient:
+    from ovos_utils import create_daemon
+    from tornado import web, ioloop
+    from ovos_messagebus.event_handler import MessageBusEventHandler
+
+    INTERNAL_PORT = 9987  # can be anything, wanted to differentiate from standard ovos-bus
+
+    routes = [("/core", MessageBusEventHandler)]
+    application = web.Application(routes)
+    application.listen(INTERNAL_PORT, "127.0.0.1")
+    create_daemon(ioloop.IOLoop.instance().start)
+
+    bus = MessageBusClient(host="127.0.0.1", port=INTERNAL_PORT)
+    bus.run_in_thread()
+    return bus
+
+
+# TODO - add a flag to use FakeBus instead of real websocket
 @click.command(help="connect to HiveMind")
 @click.option("--host", help="hivemind host", type=str, default="")
 @click.option("--key", help="Access Key", type=str, default="")
@@ -67,13 +86,17 @@ def connect(host, key, password, port, selfsigned, siteid):
         LOG.error(f"ws://{host} or wss://{host}")
         exit(1)
 
+    # TODO - flag for fakebus
+    internal_bus = launch_bus_daemon() or FakeBus()
+
     # connect to hivemind
     bus = HiveMessageBusClient(key=key,
                                password=password,
                                port=port,
                                host=host,
-                               useragent="VoiceSatelliteV0.3.0",
-                               self_signed=selfsigned)
+                               useragent="VoiceSatelliteV0.3.1",
+                               self_signed=selfsigned,
+                               internal_bus=internal_bus)
     bus.connect(site_id=siteid)
 
     # create Audio Output interface (TTS/Music)
