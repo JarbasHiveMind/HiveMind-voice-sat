bd7d795b67be3ff95e145c23b37e23a9707a4a72
JarbasAl <JarbasAl@users.noreply.github.com>
2024-10-25 03:14:40 +0000

    Increment Version to 2.0.0a15

--- hivemind_voice_satellite/version.py
+++ hivemind_voice_satellite/version.py
@@ -1,7 +1,6 @@
-# The following lines are replaced during the release process.
 # START_VERSION_BLOCK
 VERSION_MAJOR = 2
 VERSION_MINOR = 0
 VERSION_BUILD = 0
-VERSION_ALPHA = 14
+VERSION_ALPHA = 15
 # END_VERSION_BLOCK
